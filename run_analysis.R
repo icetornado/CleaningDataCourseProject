483745a576c7bb50e3fb662cba3f09da4fe5399d
icetornado <ttran@infina.net>
2015-08-21 12:32:28 -0400

    More comments on run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,9 @@
 library("dplyr")
 
+## This function takes a list of varibale names as an input
+## cleaning and replacing variable names with better ones
 doCleanNames <- function(x) {
-    #cleanNames <- gsub("\\(\\)", "", x)
+    ## replacing non-standard characters, or obscured abbreviations in variable names 
     cleanNames <- gsub("mean\\(\\)", "Mean", x)
     cleanNames <- gsub("std\\(\\)", "Std", cleanNames)
     cleanNames <- gsub("Acc", "Accelerometer", cleanNames)
@@ -13,6 +15,10 @@ doCleanNames <- function(x) {
     return(cleanNames)
 }
 
+## This function join data from three text files
+##  "y_...", "X_..." and "subject_...".
+## Input parameter folder takes either "train" or "set" value.
+## depending on the input parameter, the function will look for the set of three text files in appropriate folder and join data together
 doJoinTables <- function(folder) {
     ## replace activity number with activity name in y_"file"
     activities <- read.csv(paste("./UCI HAR Dataset", folder, paste("y_", folder, ".txt", sep = ""), sep = "/"), header = FALSE, sep = " ")
@@ -42,17 +48,24 @@ doJoinTables <- function(folder) {
 rawActivities <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
 ## get column names from features.txt
-## cleanup names up to compliance with make.names()
 rawNames <- read.table("./UCI HAR Dataset/features.txt")
+## pickup names with "-mean()" or "-std()"
 measureNames <- rawNames[grep("-mean\\(\\)|-std\\(\\)", rawNames$V2),]
+## get a list of cleaned names and attach it to dataframe
 measureNames <- transform(measureNames, measureName = doCleanNames(measureNames$V2))
 
+## get train data 
 trainData <- doJoinTables('train')
+## get test data
 testData <- doJoinTables('test')
+## combine them together
 bigData <- rbind(trainData, testData)
 
 bigData <- group_by(bigData, subjectID, activity)
-# register mean call back for summarise_each to call
+## register mean call back for summarise_each to call
 funs(mean, "mean", mean(., na.rm = TRUE))
+## perform mean calculation on each variables
 tidyData <- summarise_each(bigData, funs(mean))
+
+## output tidy data to a text file
 write.table(tidyData, file = "tidydata.txt", row.names = FALSE)
